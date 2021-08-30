ef9e4f6b44ce8e12ddbaada890a13e5f8ae554f6
Fabian Kirchmann / BBS2H20A <Fabian.Kirchmann@edu.bib.de>
2021-08-30 08:37:37 +0200

    switched zu einer eingebe funktion

--- taschenrechner/taschenrechner/ViewController.swift
+++ taschenrechner/taschenrechner/ViewController.swift
@@ -58,48 +58,13 @@ class ViewController: UIViewController {
     }
     @IBOutlet weak var label: UILabel!
     
-    @IBAction func komma(_ sender: UIButton) {
-        label.text=label.text!+"."
-    }
-    @IBAction func b0(_ sender: UIButton) {
-        label.text = label.text!+"0"
+    
+    @IBAction func eingabe(_ sender: UIButton) {
+        label.text = label.text! + sender.title(for: .normal)!
+        
     }
     
-    @IBAction func b1(_ sender: UIButton) {
-        label.text = label.text!+"1"
-    }
     
-    @IBAction func b2(_ sender: UIButton) {
-        label.text = label.text!+"2"
-    }
-    
-    @IBAction func b3(_ sender: UIButton) {
-        label.text = label.text!+"3"
-    }
-    
-    @IBAction func b4(_ sender: UIButton) {
-        label.text = label.text!+"4"
-    }
-    
-    @IBAction func b5(_ sender: UIButton) {
-        label.text = label.text!+"5"
-    }
-    
-    @IBAction func b6(_ sender: UIButton) {
-        label.text = label.text!+"6"
-    }
-    
-    @IBAction func b7(_ sender: UIButton) {
-        label.text = label.text!+"7"
-    }
-    
-    @IBAction func b8(_ sender: UIButton) {
-        label.text = label.text!+"8"
-    }
-    
-    @IBAction func b9(_ sender: UIButton) {
-        label.text = label.text!+"9"
-    }
     
     
     
